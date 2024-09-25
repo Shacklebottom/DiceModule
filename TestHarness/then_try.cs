906f9b4203ec089944db537d7f0c900fff6a6f5f
Shacklebottom <last0illusion@gmail.com>
2024-09-25 02:05:55 -0500

    still playing in the testharness :)

--- TestHarness/then_try.cs
+++ TestHarness/then_try.cs
@@ -7,24 +7,40 @@ Console.WriteLine("turtles begin" + "\n");
 
 var facets = new List<IFacet>();
 
-for (int i = 0; i < 5; i++)
+
+for (int i = 1; i < 5; i++)
 {
-    facets.Add(FacetFactory.CreateFacet(Symbols.Moon));
+    facets.Add(FacetFactory.CreateFacet((Numerics)i));
 }
-facets.Add(FacetFactory.CreateFacet(Symbols.Sun));
 
 Die die = new(facets);
 var result = die.Roll();
 
-Console.WriteLine($"You rolled the {result.Face}!");
+Console.WriteLine($"You rolled {result.Face}!");
 
-if ((Symbols)result.Face == Symbols.Sun)
-{
-    Console.WriteLine("\n" + "You won! :)");
-}
-else
-{
-    Console.WriteLine("\n" + "You lost! :(");
-}
+#region SYMBOLS
+//for (int i = 0; i < 5; i++)
+//{
+//    facets.Add(FacetFactory.CreateFacet(Symbols.Moon));
+//}
+//facets.Add(FacetFactory.CreateFacet(Symbols.Sun));
+
+//Die die = new(facets);
+//var result = die.Roll();
+
+//Console.WriteLine($"You rolled the {result.Face}!");
+
+//if ((Symbols)result.Face == Symbols.Sun)
+//{
+//    Console.WriteLine("\n" + "You won! :)");
+//}
+//else
+//{
+//    Console.WriteLine("\n" + "You lost! :(");
+//}
+#endregion
+
 
 Console.WriteLine("\n" + "turtles end");
+
+
