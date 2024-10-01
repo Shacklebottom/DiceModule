8c4d615e4ca0296025d823ffd53c742741df29b7
Shacklebottom <last0illusion@gmail.com>
2024-09-30 21:41:25 -0500

    added RollResult property and an Equals().

--- GambitDomain/Objects/Die.cs
+++ GambitDomain/Objects/Die.cs
@@ -1,5 +1,6 @@
 ﻿using GambitDomain.Extensions;
 using GambitDomain.Interfaces;
+using System.Runtime.CompilerServices;
 
 namespace GambitDomain.Objects
 {
@@ -7,6 +8,17 @@ namespace GambitDomain.Objects
     {
         private readonly IEnumerable<IFacet> _facets = facets;
 
-        public IFacet Roll() => _facets.Randomize().First();
+        public IFacet RollResult
+        {
+            get { return _facets.Randomize().First(); }
+        }
+
+        public IFacet Roll() => RollResult;
+
+        public bool Equals(IFacet facet)
+        {
+            if (this == facet) return true;
+            else return false;
+        }
     }
 }
